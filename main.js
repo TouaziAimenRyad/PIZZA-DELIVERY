3d662a27d5f88f5a9c818654a6a8863cd44e4c9d
TOUAZI AIMEN RYAD <touazi.aimen@gmail.com>
2022-04-04 21:33:33 +0200

    adding main file

---
+++ main.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const routes=require('./routes/routes')
+const server=express()
+
+server.use(express.static('public'));
+
+
+server.use(express.urlencoded({ extended: true }));
+server.set('view engine','ejs')
+
+server.use('/menu',routes)
+
+server.use((req, res) => {
+    res.status(404).send('404')
+})
+
+server.listen(8000)
