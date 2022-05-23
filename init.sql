2ff200ff4ec49a0e601de964741618c2ae592b84
TOUAZI AIMEN RYAD <touazi.aimen@gmail.com>
2022-05-23 23:45:07 +0200

    all design done make nav responsive and error management

--- init.sql
+++ init.sql
@@ -14,7 +14,7 @@ DROP TABLE IF EXISTS entree;
 DROP TABLE IF EXISTS pizza;
 DROP TABLE IF EXISTS boisson;
 DROP TABLE IF EXISTS ingrediant;
-
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE sauce  (
     nom VARCHAR(255) PRIMARY KEY,
@@ -56,45 +56,9 @@ CREATE TABLE pizza_ing(
     PRIMARY KEY(pizza_nom,ing_nom)
 );
 
-CREATE TABLE pizza_cmd(
-    nom VARCHAR(255) ,
-    taille VARCHAR(2),
-    prix FLOAT,
-    PRIMARY KEY(nom,taille)
-);
 
-CREATE TABLE boisson_cmd(
-    nom VARCHAR(255) ,
-    taille VARCHAR(10),
-    prix FLOAT,
-    PRIMARY KEY(nom,taille)
-);
 
-CREATE TABLE menu_cmd(
-    id VARCHAR(255) PRIMARY KEY,
-    type VARCHAR(255),
-    pizza VARCHAR(255) REFERENCES pizza_cmd(nom),
-    boisson VARCHAR(255) REFERENCES boisson_cmd(nom),
-    entree VARCHAR(255) REFERENCES entree(nom),
-    sauce VARCHAR(255) REFERENCES sauce(nom),
-    prix FLOAT
-    
-);
 
-CREATE TABLE commande(
-    id SERIAL PRIMARY KEY,
-    nom VARCHAR(255),
-    prenom VARCHAR(255),
-    adresse VARCHAR(512),
-    pizza VARCHAR(255) REFERENCES pizza_cmd(nom),
-    entree VARCHAR(255) REFERENCES entree(nom),
-    sauce VARCHAR(255) REFERENCES sauce(nom),
-    boisson VARCHAR(255) REFERENCES boisson_cmd(nom),
-    pizza_perso VARCHAR(1024),
-    menu VARCHAR(255) REFERENCES menu_cmd(id),
-    prix_total FLOAT
-
-);
 
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
@@ -111,10 +75,6 @@ ALTER TABLE pizza OWNER TO projet_user;
 ALTER TABLE boisson OWNER TO projet_user;
 ALTER TABLE ingrediant OWNER TO projet_user;
 ALTER TABLE pizza_ing OWNER TO projet_user;
-ALTER TABLE pizza_cmd OWNER TO projet_user;
-ALTER TABLE boisson_cmd OWNER TO projet_user;
-ALTER TABLE menu_cmd OWNER TO projet_user;
-ALTER TABLE commande OWNER TO projet_user;
 ALTER TABLE users OWNER TO projet_user;
 
 
@@ -125,10 +85,15 @@ INSERT INTO sauce(nom,prix) VALUES
 
 INSERT INTO entree(nom,description,prix) VALUES 
 ('Breadsticks Extra Mozzarella','Comme le Breadsticks Mozzarella, avec en plus de la Mozzarella gratinée par-dessus! Servis avec une sauce au choix.',6.5),
-('Breadsticks mozzarella ','8 Bâtonnets de pâte à pizza garnis de mozzarella. Servis avec une délicieuse sauce au choix.',5.0),
-('Salade Caesar','Salade verte, tomates fraîches, filet de poulet rôti et mariné, Parmigiano Reggiano AOP, tranche de pain à l’ail et une sauce Caesar',7.0),
+('Breadsticks mozzarella','8 Bâtonnets de pâte à pizza garnis de mozzarella. Servis avec une délicieuse sauce au choix.',5.0),
+('Salade Caesar','Salade verte, tomates fraîches, filet de poulet rôti et mariné, Parmigiano Reggiano AOP, tranche de pain à lail et une sauce Caesar',7.0),
 ('Chicken Wings Epicées','Recette très épicée : 6 ailes de poulet marinées',5.0),
-('Chicken Wings','Recette originale : 6 ailes de poulet marinées.',5.0);
+('Chicken Wings','Recette originale : 6 ailes de poulet marinées.',5.0),
+('Potatoes','Des Potatoes cuites au four. Servies avec une sauce au choix.',3.0),
+('Chicken Croqs','8 Bouchées Panées au Poulet. Servis avec une délicieuse sauce au choix.',5.0),
+('Salade Méditerranéenne','Salade verte, tomates fraîches, champignons frais, Parmigiano Reggiano AOP, olives noires, tranche de pain à lail et vinaigrette balsamique',7.0);
+
+
 
 INSERT INTO pizza(nom,description,prix) VALUES 
 ('4 Fromages','Sauce tomate à l origan , mozzarella, fromage de chèvre, emmental et Fourme d Ambert AOP',8.5),
@@ -138,33 +103,21 @@ INSERT INTO pizza(nom,description,prix) VALUES
 ('Chicken Barbecue','Sauce barbecue, mozzarella, filet de poulet rôti, oignons rouges frais, champignons frais et poivrons verts frais',8),
 ('Orientale','Sauce tomate à l origan, mozzarella, merguez et champignons frais',7),
 ('Margherita','Sauce tomate à l origan et mozzarella',5),
-('Nordique','Crème fraîche légère, mozzarella et saumon fumé de Norvège',8.5);
+('Nordique','Crème fraîche légère, mozzarella et saumon fumé de Norvège',8.5),
+('BPM','Sauce barbecue, mozzarella, emietté au boeuf, filet de poulet rôti et merguez',8.5),
+('Pepperoni Lovers','Sauce tomate à lorigan, mozzarella et saucisse pepperoni',8.0),
+('Queen','Sauce tomate à lorigan, mozzarella, jambon et champignons frais',7.0),
+('Chevre Miel','Crème fraîche légère, mozzarella, fromage de chèvre, miel',7.0),
+('Texane Barbecue','Sauce barbecue, mozzarella, jambon, emietté au boeuf, lardons, champignons frais et oignons rouges frais',8.0),
+('Campagnarde','Crème fraîche légère, mozzarella, lardons, oignons rouges frais et champignons frais',8.0),
+('Kasbah','Sauce tomate à lorigan, mozzarella, émincé de poulet kebab, tomates fraîches, oignons rouges frais et sauce blanche kebab',8.0),
+('Samouraï','Sauce tomate à lorigan, mozzarella, merguez, filet de poulet rôti, oignons rouges frais, Sauce Samouraï',8.0),
+('Chicken Parmesan','Sauce tomate à lorigan, mozzarella, filet de poulet rôti, tomates fraîches, parmigiano reggiano AOP',8.5);
 
-INSERT INTO pizza_cmd(nom,taille,prix) VALUES 
-('4 Fromages','S',8.5),
-('4 Fromages','M',10.5),
-('4 Fromages','L',12.5),
-('Montagnarde','S',8.5),
-('Montagnarde','M',10.5),
-('Montagnarde','L',12.5),
-('Suprême','S',8.5),
-('Suprême','M',10.5),
-('Suprême','L',12.5),
-('Raclette','S',8.0),
-('Raclette','M',10.0),
-('Raclette','L',12.0),
-('Chicken Barbecue','S',8.0),
-('Chicken Barbecue','M',10.0),
-('Chicken Barbecue','L',12.0),
-('Orientale','S',7.0),
-('Orientale','M',9.0),
-('Orientale','L',11.0),
-('Margherita','S',5.0),
-('Margherita','M',7.0),
-('Margherita','L',9.0),
-('Nordique','S',8.5),
-('Nordique','M',10.5),
-('Nordique','L',12.5);
+
+
+
+
 
 
 
@@ -175,52 +128,36 @@ INSERT INTO boisson(nom,prix) VALUES
 ('Oasis Tropical',2.0),
 ('Schweeps Agrumes',2.0),
 ('Sprite Original',2.0),
-('Schweeps Orange',2.0);
-
-INSERT INTO boisson_cmd(nom,taille,prix) VALUES 
-('Coca-Cola Original','33CL',2.0),
-('Coca-Cola Original','1L',3.4),
-('Coca-Cola Original','2L',3.8),
-('Coca-Cola sans sucres','33CL',2.0),
-('Coca-Cola sans sucres','1L',3.4),
-('Coca-Cola sans sucres','2L',3.8),
-('Coca-Cola Cherry','33CL',2.0),
-('Coca-Cola Cherry','1L',3.4),
-('Coca-Cola Cherry','2L',3.8),
-('Oasis Tropical','33CL',2.0),
-('Oasis Tropical','1L',3.4),
-('Oasis Tropical','2L',3.8),
-('Schweeps Agrumes','33CL',2.0),
-('Schweeps Agrumes','1L',3.4),
-('Schweeps Agrumes','2L',3.8),
-('Sprite Original','33CL',2.0),
-('Sprite Original','1L',3.4),
-('Sprite Original','2L',3.8),
-('Schweeps Orange','33CL',2.0),
-('Schweeps Orange','1L',3.4),
-('Schweeps Orange','2L',3.8);
+('Fuze Tea',2.0),
+('Orangina',2.0),
+('Oasis Pomme Cassis Framboise',2.0);
 
 
 INSERT INTO ingrediant(nom,type,prix) VALUES 
-('sauce tomate','sauce',0.4),
-('crème fraîche','sauce',0.4),
-('sauce barbecue','sauce',0.4),
-('mozzarella','fromage',0.4),
-('fromage de chèvre','fromage',0.4),
-('Ambert','fromage',0.4),
-('raclette','fromage',0.4),
-('emmental','fromage',0.4),
-('jambon','viande',0.4),
-('boeuf','viande',0.4),
-('pepperoni','viande',0.4),
-('lardons','viande',0.4),
-('poulet','viande',0.4),
-('saumon','viande',0.4),
-('merguez','viande',0.4),
-('champignons','legume',0.4),
-('oignons rouges','legume',0.4),
-('poivrons verts','legume',0.4),
-('pommes de terre','legume',0.4);
+('sauce tomate','sauce',0),
+('crème fraîche','sauce',0),
+('sauce barbecue','sauce',0),
+('mozzarella','fromage',2.0),
+('fromage de chèvre','fromage',0.5),
+('Ambert','fromage',1.0),
+('sauce blanche','fromage',0.5),
+('parmigiano','fromage',2.0),
+('raclette','fromage',1.0),
+('emmental','fromage',0.5),
+('jambon','viande',1.0),
+('boeuf','viande',1.0),
+('kebab','viande',1.0),
+('pepperoni','viande',2.0),
+('lardons','viande',0.5),
+('poulet','viande',1.0),
+('saumon','viande',1.5),
+('merguez','viande',1.0),
+('miel','legume',1.5),
+('tomate','legume',0.5),
+('champignons','legume',0.5),
+('oignons rouges','legume',0.5),
+('poivrons verts','legume',0.5),
+('pommes de terre','legume',0.5);
 
 
 INSERT INTO pizza_ing(pizza_nom,ing_nom) VALUES 
@@ -229,11 +166,13 @@ INSERT INTO pizza_ing(pizza_nom,ing_nom) VALUES
 ('4 Fromages','fromage de chèvre'),
 ('4 Fromages','Ambert'),
 ('4 Fromages','emmental'),
+
 ('Montagnarde','crème fraîche'),
 ('Montagnarde','mozzarella'),
 ('Montagnarde','jambon'),
 ('Montagnarde','raclette'),
 ('Montagnarde','champignons'),
+
 ('Suprême','sauce tomate'),
 ('Suprême','mozzarella'),
 ('Suprême','boeuf'),
@@ -241,24 +180,83 @@ INSERT INTO pizza_ing(pizza_nom,ing_nom) VALUES
 ('Suprême','champignons'),
 ('Suprême','oignons rouges'),
 ('Suprême','poivrons verts'),
+
 ('Raclette','crème fraîche'),
 ('Raclette','mozzarella'),
 ('Raclette','pommes de terre'),
 ('Raclette','lardons'),
 ('Raclette','raclette'),
+
 ('Chicken Barbecue','sauce barbecue'),
 ('Chicken Barbecue','mozzarella'),
 ('Chicken Barbecue','poulet'),
 ('Chicken Barbecue','oignons rouges'),
 ('Chicken Barbecue','poivrons verts'),
 ('Chicken Barbecue','champignons'),
+
 ('Orientale','sauce tomate'),
 ('Orientale','mozzarella'),
 ('Orientale','merguez'),
 ('Orientale','champignons'),
+
 ('Margherita','sauce tomate'),
 ('Margherita','mozzarella'),
+
 ('Nordique','crème fraîche'),
 ('Nordique','mozzarella'),
-('Nordique','saumon');
+('Nordique','saumon'),
+
+('BPM','sauce barbecue'),
+('BPM','boeuf'),
+('BPM','poulet'),
+('BPM','merguez'),
+('BPM','mozzarella'),
+
+('Pepperoni Lovers','sauce tomate'),
+('Pepperoni Lovers','mozzarella'),
+('Pepperoni Lovers','pepperoni'),
+
+('Queen','sauce tomate'),
+('Queen','jambon'),
+('Queen','champignons'),
+('Queen','mozzarella'),
+
+('Chevre Miel','crème fraîche'),
+('Chevre Miel','fromage de chèvre'),
+('Chevre Miel','mozzarella'),
+('Chevre Miel','miel'),
+
+('Texane Barbecue','sauce barbecue'),
+('Texane Barbecue','jambon'),
+('Texane Barbecue','boeuf'),
+('Texane Barbecue','mozzarella'),
+('Texane Barbecue','lardons'),
+('Texane Barbecue','champignons'),
+('Texane Barbecue','oignons rouges'),
+
+('Campagnarde','crème fraîche'),
+('Campagnarde','mozzarella'),
+('Campagnarde','lardons'),
+('Campagnarde','oignons rouges'),
+('Campagnarde','champignons'),
+
+('Kasbah','sauce tomate'),
+('Kasbah','mozzarella'),
+('Kasbah','merguez'),
+('Kasbah','kebab'),
+('Kasbah','tomate'),
+('Kasbah','oignons rouges'),
+('Kasbah','sauce blanche'),
+
+('Samouraï','sauce tomate'),
+('Samouraï','mozzarella'),
+('Samouraï','merguez'),
+('Samouraï','poulet'),
+('Samouraï','oignons rouges'),
+
+('Chicken Parmesan','sauce tomate'),
+('Chicken Parmesan','mozzarella'),
+('Chicken Parmesan','poulet'),
+('Chicken Parmesan','parmigiano'),
+('Chicken Parmesan','tomate');
 
